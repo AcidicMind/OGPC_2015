a43d6b89dc7b981fc5cda882968ff4c2f589b3df
xx_Archibald_xx <riley.d.waugh@gmail.com>
2015-02-10 14:46:07 -0800

    Update ofApp.h
    same description as the last one that I submitted

--- src/ofApp.h
+++ src/ofApp.h
@@ -1,38 +1,53 @@
 #pragma once
 #include "ofMain.h"
 
+typedef vector< vector<int> > Matrix;
+typedef vector<int> Row;
+
+class Enemy
+{
+    public:
+    void aiMovement(int key,int playerx,int playery,const size_t N,Matrix matrix);// typedef not declared yet
+    void test(int key);
+    int direction=0;
+    int EnemyX = 9;
+    int EnemyY = 9;
+    int health=10;
+};
+
 class board
 {
     public:
-    //Enemy enemy1;
-    int gapSize = 5;
     void boardDrawer();
     void tileSetup();
     void playerController();
-    int squareSize = 50;
     int playerx = 0;
-    int Previous = 0;
     int playery = 0;
+    int Previous = 0;
+    int PreviousEnemy = 1;
+//    int PreviousEnemy2 = 1;
+//    int PreviousEnemy3 = 1;
+//    int PreviousEnemy4 = 1;
+//    int PreviousEnemy5 = 1;
     int wallcount = 0;
     int key = 0; //key pressed
-    int keyCount = 0;//number of keys -- supposed to be only one
-    typedef vector< vector<int> > Matrix;
-    typedef vector<int> Row;
+    int keyCount = 0;//number of keys checker -- supposed to be only one
+    int exitCount = 0;//number of exits checker -- supposed to be only one
+    bool hasKey = false;//do we have the key yet???
+    int keyCheckIfItHasIt = 0;
+    const size_t N = 12; // the dimension of the matrix
+    int gapSize = 5;
+    int squareSize = 60;
     vector<int>wall;
-    const size_t N = 10; // the dimension of the matrix
     Matrix matrix;
     Matrix level;
+    Enemy enemy1;
+//    Enemy enemy2;
+//    Enemy enemy3;
+//    Enemy enemy4;
+//    Enemy enemy5;
 };
-//class Enemy
-//{
-//    public:
-//    //board board1;
-//    void aiMovement();
-//    int direction=0;
-//    int EnemyX=9;
-//    int EnemyY=9;
-//    int health=10;
-//};
+
 class ofApp : public ofBaseApp{
 
 	public:
@@ -50,5 +65,4 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
         board board1;
-        //Enemy enemy1;
 };
