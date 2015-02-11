6c533774c683045fdff5885ffa502aacc503c43f
AcidicMind <sashaleamy@hotmail.com>
2015-02-11 10:28:47 -0800

    Update ofApp.h

--- src/ofApp.h
+++ src/ofApp.h
@@ -20,7 +20,7 @@ class board
     public:
     void boardDrawer();
     void tileSetup();
-    void playerController();
+    void playerController(int key);
     int playerx = 0;
     int playery = 0;
     int Previous = 0;
@@ -31,7 +31,7 @@ class board
 //    int PreviousEnemy5 = 1;
     int moves = 0;
     int wallcount = 0;
-    int key = 0; //key pressed
+    //int key = 0; //key pressed
     int keyCount = 0;//number of keys checker -- supposed to be only one
     int exitCount = 0;//number of exits checker -- supposed to be only one
     bool hasKey = false;//do we have the key yet???
