9d519895abd15ea39feecb23a763252d2c327f15
Sam Norman Behrens <sbehrens1414@gmail.com>
2015-02-12 21:15:22 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
+
 #include "ofMain.h"
 #include "ofApp.h"
+#include <math.h>
 
 //========================================================================
 int main( ){
     board board1;
-	ofSetupOpenGL((board1.squareSize + board1.gapSize) * board1.N,(board1.squareSize + board1.gapSize) * board1.N,OF_WINDOW);			// <-------- setup the GL context
+	ofSetupOpenGL(1000,700,OF_FULLSCREEN);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
