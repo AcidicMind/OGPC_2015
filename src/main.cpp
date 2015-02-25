791b4aba10badb90e80c5ebeac3974f987d52584
AcidicMind <sashaleamy@hotmail.com>
2015-02-25 11:20:35 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,15 @@
+#include "ofMain.h"
+#include "ofApp.h"
+#include <math.h>
 
+//========================================================================
+int main( ){
+    board board1;
+	ofSetupOpenGL(1000,700,OF_FULLSCREEN);			// <-------- setup the GL context
+
+	// this kicks off the running of my app
+	// can be OF_WINDOW or OF_FULLSCREEN
+	// pass in width and height too:
+	ofRunApp(new ofApp());
+
+}
