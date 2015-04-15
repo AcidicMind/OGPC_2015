d773a630b213f51209f4e874dcd3fd1542dbdab3
softandsupple <lukemitchellnelson@gmail.com>
2015-04-15 02:01:07 -0700

    classin the place up
    new class for the menu that I'll hopefully be able to change into .h files and then take to the main project

--- src/ofApp.h
+++ src/ofApp.h
@@ -2,39 +2,30 @@
 
 #include "ofMain.h"
 
-class ofApp : public ofBaseApp{
+class menu{
 
-	public:
-		void setup();
-		void update();
-		void draw();
+public:
+    void setup();
+    void draw();
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		void singleplayer();
-		void builder();
-		void adventurer();
-
-		bool optionson =false;
+        bool optionson =false;
 		bool abouton =false;
 
-		int winheight;
-		int winwidth;
-		int LRpadding;
-		int TBpadding;
-		int interiorpadding;
-		int textsize;
-		int smalltextsize;
-		int smalltextsize2;
+		ofImage bg;
+		ofImage sound;
+		ofImage mute;
 
+		ofTrueTypeFont titlefont;
+		ofTrueTypeFont textfont;
+		ofTrueTypeFont smalltextfont;
+		ofTrueTypeFont smalltextfont2;
 
+		int winheight, winwidth, LRpadding, TBpadding, interiorpadding;
+		int textsize, smalltextsize, smalltextsize2;
 
 		ofColor Adventurerext;
 		ofColor Adventurerint;
@@ -42,30 +33,19 @@ class ofApp : public ofBaseApp{
 		ofColor Builderint;
 		ofColor Singleext;
 		ofColor Singleint;
-
-		int AdventurerExtopacity=40;
-		int AdventurerIntopacity=120;
-		int BuilderExtopacity=40;
-        int BuilderIntopacity=120;
-        int SingleExtopacity =40;
-        int SingleIntopacity =120;
-        int Aboutopacity;
-        int Optionsopacity;
-        int advred=40;
-        int bldrred=40;
-        int singred=40;
-        int allopacity =0;
-        int containeropacity =20;
-
-        int fxpercent=.5;
-        int fxpixels;
-        bool fxpressed =false;
-        int musicpercent =50;
-        int musicpixels;
-        bool musicpressed =false;
-        int masterpercent=50;
-        int masterpixels;
-        bool masterpressed =false;
+            int AdventurerExtopacity=40;
+            int AdventurerIntopacity=120;
+            int BuilderExtopacity=40;
+            int BuilderIntopacity=120;
+            int SingleExtopacity =40;
+            int SingleIntopacity =120;
+            int Aboutopacity;
+            int Optionsopacity;
+            int advred=40;
+            int bldrred=40;
+            int singred=40;
+            int allopacity =0;
+            int containeropacity =20;
 
         ofRectangle TwoPcontainer;
         ofRectangle OnePcontainer;
@@ -86,15 +66,37 @@ class ofApp : public ofBaseApp{
 		ofRectangle musicslider;
 		ofRectangle masterslider;
 
-		ofTrueTypeFont titlefont;
-		ofTrueTypeFont textfont;
-		ofTrueTypeFont smalltextfont;
-		ofTrueTypeFont smalltextfont2;
+		int fxpercent=.5;
+        int fxpixels;
+        bool fxpressed =false;
+        int musicpercent =50;
+        int musicpixels;
+        bool musicpressed =false;
+        int masterpercent=50;
+        int masterpixels;
+        bool masterpressed =false;
 
 
-		ofImage bg;
-		ofImage sound;
-		ofImage mute;
+};
 
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		void singleplayer();
+		void builder();
+		void adventurer();
 
 };
